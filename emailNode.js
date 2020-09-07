3c67f3b5171c301d64efedf3db2091c89a1a574c
Rahul home <rahulvaishnav166@gmail.com>
2020-09-07 13:49:19 +0530

    dual email service

---
+++ emailNode.js
@@ -0,0 +1,78 @@
+var cors = require('cors')
+const https = require('https');
+var express = require('express')
+var fs = require('fs')
+var app = express()
+
+// var pathCert = "/path/"
+// var privateKey = fs.readFileSync(pathCert + '/privkey.pem', 'utf8');
+// var certificate = fs.readFileSync(pathCert + '/fullchain.pem', 'utf8');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'rahulvaishnav166@gmail.com',
+        pass: 'khstztmjxbpjztcc'
+    }
+});
+var mailOptions = {
+    from: 'rahulvaishnav166@gmail.com',
+    to: 'nekvinder@gmail.com',
+    subject: 'New Query at Karnex',
+};
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', function (request, response) {
+    console.log('GET /')
+    var html = `
+    <html>
+        <body>
+            <!--<form method="post" action="http://localhost:5000">Name: 
+                <input type="text" name="name" />
+                <input type="submit" value="Submit" />
+            </form>-->
+            Working...
+        </body>
+    </html>`
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    response.end(html)
+})
+
+app.post('/', function (request, response) {
+    console.log('POST /')
+
+    if (request.body.type == "query") {
+        mailOptions.text = 'Query by : ' + request.body.name + ' of company ' + request.body.company + '\nEmail ID : ' + request.body.email + '\nContact :' + request.body.contact + '\nQuery : ' + request.body.msg + '\n';
+    } else {
+        mailOptions.text = 'Name : ' + request.body.name + '\nInfo by ' + request.body.info + '\nEmail ID : ' + request.body.email + '\nContact :' + request.body.contact + '\nQuery : ' + request.body.msg + '\nCountry : ' + request.body.country + '\n';
+    }
+    console.dir(mailOptions);
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        if (error) {
+            response.write('error'); //write a response to the client
+            console.log(error);
+        } else {
+            response.write('Sent'); //write a response to the client
+            console.log('Email sent: ' + info.response);
+        }
+        response.end('')
+    });
+})
+
+port = 5000
+app.listen(port)
+// https.createServer(app).listen(port)
+console.log(`Listening at http://localhost:${port}`)
---
+++ uploadToAws.sh
@@ -0,0 +1,10 @@
+#!/bin/bash    
+
+KEY_PATH="/atools/aws_resources/karnex/karnex.pem"
+AWS_IP="karnex.in"
+
+ssh -i $KEY_PATH ubuntu@$AWS_IP 'sudo apt install npm;sudo npm install pm2@latest -g;cd /opt;sudo mkdir karnexMailServer;sudo chmod 777 karnexMailServer/ -R;cd karnexMailServer/'
+curl --insecure -v -k --key $KEY_PATH sftp://ubuntu@$AWS_IP/opt/karnexMailServer/emailNode.js -T "emailNode.js"
+curl --insecure -v -k --key $KEY_PATH sftp://ubuntu@$AWS_IP/opt/karnexMailServer/package.json -T "package.json"
+# curl --insecure -v -k --key $KEY_PATH sftp://ubuntu@$AWS_IP/opt/kumoroMailServer/pm2runConfig.json -T "pm2runConfig.json"
+ssh -i $KEY_PATH ubuntu@$AWS_IP 'cd /opt/karnexMailServer/;npm i;sudo pm2 start emailNode.js'
